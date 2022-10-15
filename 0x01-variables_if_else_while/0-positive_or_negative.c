92647e5c0ad94071df39d12d5c678939f641dc14
clemsonexcel <excelclemson@gmail.com>
2022-10-14 19:13:19 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,22 +2,23 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *
  * main - entry point
  *
  * return: Always 0 (success)
  */
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n> 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
-	{	
+	{
 		printf("%d is zero\n", n);
 	}
 	else
