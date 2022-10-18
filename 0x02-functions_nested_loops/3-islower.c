f70606e3c65eb21bcfab3a9655f70164f2cf836f
clemsonexcel <excelclemson@gmail.com>
2022-10-18 16:33:29 -0700

    main

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * _is lower - checks for lower case
+ * @c: character to be checked.
+ * return: 1 on success, 0 otherwise
+ */
+
+int _lower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (0);
+	else
+		return (1);
+}
