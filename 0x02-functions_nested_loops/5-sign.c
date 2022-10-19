92c8b55efec3519c42674d2b5c696476e3a2dbd0
clemsonexcel <excelclemson@gmail.com>
2022-10-18 17:18:24 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print - prints the sign of a number.
+ * @n: number to be checked
+ * return: 1 if n is greater than 0
+ * return: 0 if n is equal to 0
+ * return: -1 if n is less than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else 
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
