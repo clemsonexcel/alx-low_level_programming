86c09d8ae325ed34b7319a8e7b58a0f3ffaab504
clemsonexcel <excelclemson@gmail.com>
2022-10-18 17:36:50 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - compute absolute value
+ * return:absolute value of integer
+ *@n: the integer to check
+ */
+
+ int _abs(int n)
+ {
+ 	if (n >= 0)
+	{
+		return (n);
+	}
+	else 
+		return (-n);
+}
