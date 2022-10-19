8265a96ddf393d5b1f6541b35b172491d89cece5
clemsonexcel <excelclemson@gmail.com>
2022-10-19 15:34:04 -0700

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - list multiples of natural number
- * return: 0 always .
+ * return: 0 always;
  */
 int main(void)
 {
 	int i, sum = 0;
 
-	for (i =0; i < 1024; i++)
+	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
