508dca0f9caebe8e68abb2d39b0b48276073cac0
clemsonexcel <excelclemson@gmail.com>
2022-10-18 17:21:25 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,8 +3,8 @@
  * print - prints the sign of a number.
  * @n: number to be checked
  * return: 1 if n is greater than 0
- * return: 0 if n is equal to 0
- * return: -1 if n is less than 0
+ *  0 if n is equal to 0
+ * -1 if n is less than 0
  */
 
 int print_sign(int n)
