414e6254f1e20bf492163960f905f6c22b806d20
clemsonexcel <excelclemson@gmail.com>
2022-10-20 15:48:07 -0700

    task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul  multiply 2 
+ * @a: first number
+ * @b: second number
+ * Return: the answer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
